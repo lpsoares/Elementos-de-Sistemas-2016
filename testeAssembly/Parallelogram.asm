3f10ffdbc917deea9258aafb0b289444f485bfc2
lpsoares <lpsoares@gmail.com>
2016-03-30 23:35:40 -0300

    projetos para testar a programar em assembly

---
+++ testeAssembly/Parallelogram.asm
@@ -0,0 +1,6 @@
+// Luciano Soares
+// File name: Parallelogram.asm
+
+// Desenha um paralelograma na tela quando uma tecla é pressionada.
+
+// Put your code here.
---
+++ testeAssembly/SternDiatomicSeries.asm
@@ -0,0 +1,10 @@
+// Luciano Soares
+// File name: SternDiatomicSeries.asm
+
+// Le a quantidade de valores da RAM[0] e grava a sequencia de números 
+// da Sequencia Diatomica de Stern nas posições seguintes RAM[1], RAM[2], etc....
+// Por exemplo: se RAM[0]=6
+// RAM[1]=0, RAM[2]=1, RAM[3]=1, RAM[4]=2, RAM[5]=1, RAM[6]=3
+// Maiores informações em: https://oeis.org/A002487
+
+// Put your code here.
