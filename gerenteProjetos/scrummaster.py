e5d5d4c8487d4ade77323bc3bf119aa684d1540a
Gustavo Efeiche <gustavo.efeiche@gmail.com>
2016-05-17 10:14:46 -0300

    Update scrummaster.py

--- gerenteProjetos/scrummaster.py
+++ gerenteProjetos/scrummaster.py
@@ -6,13 +6,13 @@ import random
 print("Picking Scrum Master...")
 
 #Get firebase app
-firebase = firebase.FirebaseApplication("https://elemsist.firebaseio.com")
+firebase = firebase.FirebaseApplication("https://elemsist.firebaseio.com/")
 
 #Get scrum master
 def get_scrum_master():
 
 	#Get student at random index in firebase
-	idx = random.randint(1,16)
+	idx = random.randint(1,8)
 	aluno = firebase.get("/alunos","/alunos{0}".format(idx))
 
 	#If student wasn't Scrum Master before, return student and update his/her status
