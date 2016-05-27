93f963f7edf4e9dd5b9b2804ac6c8e80858c2d3d
gabicfa <gabrielacfa@al.insper.edu.br>
2016-05-27 20:14:55 -0300

    Parte Gabi

--- Compilador de Alto Nível/src/JackTokenizer.java
+++ Compilador de Alto Nível/src/JackTokenizer.java
@@ -1,18 +1,26 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class JackTokenizer implements AllJackTokenizer{
 	
-    private ArrayList<String> vm = new ArrayList<>();
+	private ArrayList<String> vm = new ArrayList<>();
     private int currentLineIndex;
 	private String currentToken;
 	private static Scanner s;
-	private final String delim = "(){}><=+-[]	 ";
+	private final String delim = "(){}><=+-[]";
+    private String currentTokenType;
+	private String currentKeyWord;
 	private StringTokenizer st;
-	
+	private String[] keyword = new String[]{"class","constructor","function","method","field",
+			"static","var","int","char","boolean","void","true","false","null","this","let","do",
+			"if","else","while","return"};
+	private String[] symbol = new String[]{"}","}","(",")","[","]",".",",",";","+","-","*","/","&",
+			"|","<",">","=","~"};
+		
 	public String getCurrentToken(){
 		return currentToken;
 	}
@@ -55,39 +63,153 @@ public class JackTokenizer implements AllJackTokenizer{
 	}
 
 	@Override
-	public String tokenType() {
-		// TODO Auto-generated method stub
-		return null;
+	public String tokenType() { 
+		
+		currentToken =  currentToken.replaceAll("\\s","");
+		
+		if(Arrays.asList(keyword).contains(currentToken)){
+			currentTokenType = "KEYWORD";
+		}
+		
+		else if(Arrays.asList(symbol).contains(currentToken)){
+			currentTokenType = "SYMBOL";
+		}
+		
+		else if(Integer.parseInt(currentToken) >= 0){
+			currentTokenType = "INT_CONST";
+		}
+		
+		else if(currentToken.matches("[a-zA-Z]+")){
+			currentTokenType = "STRING_CONST";
+		}
+		
+		else {
+			currentTokenType = "IDENTIFIER";
+		}
+		
+		return currentTokenType;
 	}
 
 	@Override
 	public String keyWord() {
-		// TODO Auto-generated method stub
-		return null;
+		currentToken =  currentToken.replaceAll("\\s","");
+
+		if (currentTokenType == "KEYWORD"){
+	         if (currentToken.equals("class")) { 
+	        	 currentKeyWord = "CLASS";
+	         }
+	         else if(currentToken.equals("method")){
+	        	 currentKeyWord = "METHOD";
+	         }
+	         else if(currentToken.equals("function")){
+	        	 currentKeyWord = "FUNCTION";
+	         }
+	         else if(currentToken.equals("constructor")){
+	        	 currentKeyWord = "CONSTRUCTOR";
+	         }
+	         else if(currentToken.equals("int")){
+	        	 currentKeyWord = "INT";
+	         }
+	         else if(currentToken.equals("boolean")){
+	        	 currentKeyWord = "BOOLEAN";
+	         }
+	         else if(currentToken.equals("char")){
+	        	 currentKeyWord = "CHAR";
+	         }
+	         else if(currentToken.equals("void")){
+	        	 currentKeyWord = "VOID";
+	         }
+	         else if(currentToken.equals("var")){
+	        	 currentKeyWord = "VAR";
+	         }
+	         else if(currentToken.equals("static")){
+	        	 currentKeyWord = "STATIC";
+	         }
+	         else if(currentToken.equals("field")){
+	        	 currentKeyWord = "FIELD";
+	         }
+	         else if(currentToken.equals("let")){
+	        	 currentKeyWord = "LET";
+	         }
+	         else if(currentToken.equals("do")){
+	        	 currentKeyWord = "DO";
+	         }
+	         else if(currentToken.equals("if")){
+	        	 currentKeyWord = "IF";
+	         }
+	         else if(currentToken.equals("else")){
+	        	 currentKeyWord = "ELSE";
+	         }
+	         else if(currentToken.equals("while")){
+	        	 currentKeyWord = "WHILE";
+	         }
+	         else if(currentToken.equals("return")){
+	        	 currentKeyWord = "RETURN";
+	         }
+	         else if(currentToken.equals("true")){
+	        	 currentKeyWord = "TRUE";
+	         }
+	         else if(currentToken.equals("false")){
+	        	 currentKeyWord = "FALSE";
+	         }
+	         else if(currentToken.equals("null")){
+	        	 currentKeyWord = "NULL";
+	         }
+	         else if(currentToken.equals("this")){
+	        	 currentKeyWord = "THIS";
+	         }
+		}
+		
+		else{
+			currentKeyWord = "ERROR";
+		}
+		return currentKeyWord;       
 	}
 
 	@Override
 	public char symbol() {
-		// TODO Auto-generated method stub
-		return 0;
+		currentToken =  currentToken.replaceAll("\\s","");
+
+		if (currentTokenType == "SYMBOL"){
+			return currentToken.charAt(0);
+		}
+		else{
+			return 0;
+		}
 	}
 
 	@Override
 	public String indetifier() {
-		// TODO Auto-generated method stub
-		return null;
+		if (currentTokenType == "IDENTIFIER"){
+			return currentToken;
+		}
+		else{
+			return null;
+		}
 	}
 
 	@Override
 	public int intVal() {
-		// TODO Auto-generated method stub
-		return 0;
+		currentToken =  currentToken.replaceAll("\\s","");
+
+		if (currentTokenType == "INT_CONST"){
+			return Integer.parseInt(currentToken);
+		}
+		else{
+			return 0;
+		}
 	}
 
 	@Override
 	public String stringVal() {
-		// TODO Auto-generated method stub
-		return null;
+		currentToken =  currentToken.replaceAll("\\s","");
+
+		if (currentTokenType == "STRING_CONST"){
+			return currentToken;
+		}
+		else{
+			return null;
+		}
 	}
 	
 }
