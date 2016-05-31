8fe875e711fe1582a2f7d586d351d444fefd72e4
Pedro Cunial <pedrocc4@al.insper.edu.br>
2016-05-31 10:12:56 -0300

    A principio só falta o var count

--- Compilador/CompiladorJava/SymbolTable.java
+++ Compilador/CompiladorJava/SymbolTable.java
@@ -1,13 +1,29 @@
+import java.util.*;
+
+
 public class SymbolTable {
+	HashMap<String, String[]> classScope;
+	HashMap<String, String[]> methodScope;
+	int staticIndex;
+	int fieldIndex;
+	int varIndex;
+	int argIndex;
 
 	public SymbolTable() {
 		// Creates a new empty symbol table
+		this.classScope = new HashMap<String, String[]>();
+		this.methodScope = new HashMap<String, String[]>();
+		this.staticIndex = 0;
+		this.fieldIndex = 0;
+		this.varIndex = 0;
+		this.argIndex = 0;
 
 	}
 
 	public void startSubroutine() {
 		// Starts a new subroutine scope
 		// (i.e., resets the subroutine's symbol table)
+		this.methodScope.clear();
 
 	}
 
@@ -16,6 +32,27 @@ public class SymbolTable {
 		// and kind and assigns it a running index.
 		// STATIC and FIELD identifiers have a class scope,
 		// while ARG and VAR hve a subroutine scope
+		if(kind == "STATIC") {
+			String[] value = {type, kind, String.valueOf(this.staticIndex)};
+			this.classScope.put(name, value);
+			this.staticIndex++;
+
+		} else if(kind == "FIELD") {
+			String[] value = {type, kind, String.valueOf(this.fieldIndex)};
+			this.classScope.put(name, value);
+			this.fieldIndex++;
+
+		} else if(kind == "ARG") {
+			String[] value = {type, kind, String.valueOf(this.argIndex)};
+			this.methodScope.put(name, value);
+			this.argIndex++;
+
+		} else if(kind == "VAR") {
+			String[] value = {type, kind, String.valueOf(this.varIndex)};
+			this.methodScope.put(name, value);
+			this.varIndex++;
+
+		}
 
 	}
 
@@ -30,20 +67,32 @@ public class SymbolTable {
 		// Returns the kind of the named identifier in the
 		// scope. If the identifier is unknown in the current scope,
 		// returns NONE
-
-		return null;
+		if(methodScope.get(name) != null) {
+			return (methodScope.get(name)[1]);
+		} else if(classScope.get(name) != null) {
+			return (classScope.get(name)[1]);
+		}
+		return "NONE";
 	}
 
 	public String typeOf(String name) {
 		// Returns the type of the named identifier in the
 		// current scope
-
+		if(methodScope.get(name) != null) {
+			return (methodScope.get(name)[0]);
+		} else if(classScope.get(name) != null) {
+			return (classScope.get(name)[0]);
+		}
 		return null;
 	}
 
 	public int indexOf(String name) {
 		// Returns the index assigned to the named identifier
-
-		return 0;
+		if(methodScope.get(name) != null) {
+			return Integer.valueOf(methodScope.get(name)[2]);
+		} else if(classScope.get(name) != null) {
+			return Integer.valueOf(classScope.get(name)[2]);
+		}
+		return -1;
 	}
 }
