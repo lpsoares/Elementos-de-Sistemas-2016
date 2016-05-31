6cdd9ac1bde48a8a623bf55ac8b61c56d6ee9905
Pedro Cunial <pedrocc4@al.insper.edu.br>
2016-05-31 09:12:23 -0300

    definindo os métodos

--- Compilador/CompiladorJava/SymbolTable.java
+++ Compilador/CompiladorJava/SymbolTable.java
@@ -1,3 +1,36 @@
 public class SymbolTable {
 	
+	public SymbolTable() {
+	
+	}
+	
+	public void startSubroutine() {
+	
+	}
+	
+	public void Define(String name, String type, String kind) {
+		
+		
+	}
+	
+	public int VarCount(String kind) {
+		
+		
+	}
+	
+	public String KindOf(String name) {
+		
+		
+	}
+	
+	public String TypeOf(String name) {
+		
+		
+	}
+	
+	public int IndexOf(String name) {
+		
+		
+	}
+	
 }
