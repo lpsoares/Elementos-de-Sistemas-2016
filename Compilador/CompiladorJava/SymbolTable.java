7c472884b61ffef75885b3e911a5eff374514606
Pedro Cunial <pedrocc4@al.insper.edu.br>
2016-05-31 09:18:49 -0300

    deixei o nome dos métodos começando em minúsculo (assim como deveria ser...) e adicionei comentários explicando o escopo de cada método.

--- Compilador/CompiladorJava/SymbolTable.java
+++ Compilador/CompiladorJava/SymbolTable.java
@@ -1,35 +1,45 @@
 public class SymbolTable {
 	
 	public SymbolTable() {
+		// Creates a new empty symbol table 
 	
 	}
 	
 	public void startSubroutine() {
+		// Starts a new subroutine scope 
+		// (i.e., resets the subroutine's symbol table)
 	
 	}
 	
-	public void Define(String name, String type, String kind) {
-		
-		
-	}
-	
-	public int VarCount(String kind) {
-		
+	public void define(String name, String type, String kind) {
+		// Defines a new identifier of a given name, type
+		// and kind and assigns it a running index.
+		// STATIC and FIELD identifiers have a class scope,
+		// while ARG and VAR hve a subroutine scope
 		
 	}
 	
-	public String KindOf(String name) {
-		
-		
-	}
-	
-	public String TypeOf(String name) {
-		
+	public int varCount(String kind) {
+		// Returns the number of variables of a given kind
+		// already defined in the current scope
 		
 	}
 	
-	public int IndexOf(String name) {
+	public String kindOf(String name) {
+		// Returns the kind of the named identifier in the
+		// scope. If the identifier is unknown in the current scope,
+		// returns NONE
 		
+	}
+	
+	public String typeOf(String name) {
+		// Returns the type of the named identifier in the 
+		// current scope
+		
+	}
+	
+	public int indexOf(String name) {
+		// Returns the index assigned to the named identifier
 		
 	}
 	
