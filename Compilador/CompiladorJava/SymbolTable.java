097f342343a317d7994314879fcfed8fa94cef8e
Pedro Cunial <pedrocc4@al.insper.edu.br>
2016-05-31 09:26:54 -0300

    corrigi os erros relativos ao retorno padrão das funções e compilei o código pela primeira vez

--- Compilador/CompiladorJava/SymbolTable.java
+++ Compilador/CompiladorJava/SymbolTable.java
@@ -1,46 +1,49 @@
 public class SymbolTable {
-	
+
 	public SymbolTable() {
-		// Creates a new empty symbol table 
-	
+		// Creates a new empty symbol table
+
 	}
-	
+
 	public void startSubroutine() {
-		// Starts a new subroutine scope 
+		// Starts a new subroutine scope
 		// (i.e., resets the subroutine's symbol table)
-	
+
 	}
-	
+
 	public void define(String name, String type, String kind) {
 		// Defines a new identifier of a given name, type
 		// and kind and assigns it a running index.
 		// STATIC and FIELD identifiers have a class scope,
 		// while ARG and VAR hve a subroutine scope
-		
+
 	}
-	
+
 	public int varCount(String kind) {
 		// Returns the number of variables of a given kind
 		// already defined in the current scope
-		
+
+		return 0;
 	}
-	
+
 	public String kindOf(String name) {
 		// Returns the kind of the named identifier in the
 		// scope. If the identifier is unknown in the current scope,
 		// returns NONE
-		
+
+		return null;
 	}
-	
+
 	public String typeOf(String name) {
-		// Returns the type of the named identifier in the 
+		// Returns the type of the named identifier in the
 		// current scope
-		
+
+		return null;
 	}
-	
+
 	public int indexOf(String name) {
 		// Returns the index assigned to the named identifier
-		
+
+		return 0;
 	}
-	
 }
